0ff00bedc65a335386dc9f80efb737181504d817
nav-commits <53131408+nav-commits@users.noreply.github.com>
2020-12-17 16:32:11 -0500

    commit 9

--- Login and Register/Assets/Scripts/Register.cs
+++ Login and Register/Assets/Scripts/Register.cs
@@ -1,9 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
 
-public class Register : MonoBehaviour
-{
+
+public class Register : MonoBehaviour { 
+
+public GameObject username;
+public GameObject email;
+public GameObject password;
+
+private string Username;
+private string Email;
+private string Password;
+private string Form;
+private bool Emailvalid = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +28,20 @@ public class Register : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (username.GetComponent<InputField>().isFocused)
+            {
+                email.GetComponent<InputField>().Select();
+            }
+        }
+    
+        Username = username.GetComponent<InputField>().text;
+        Email = email.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+
     }
+
+
 }
+
